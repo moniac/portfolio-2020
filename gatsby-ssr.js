4b316d2addaba0b54278dece1d555f692d95638b
Mohammed 'Mo' Mulazada <thisismoniac@gmail.com>
2020-05-06 11:30:42 +0200

    fix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,16 @@
-exports.onRenderBody = ({ setBodyAttributes }) => {
+import React from "react"
+
+exports.onRenderBody = ({ setBodyAttributes, setHeadComponents }) => {
   setBodyAttributes({
     className: "no-js",
   })
+  setHeadComponents([
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `window.addEventListener("load", () => {
+    document.body.className = document.body.className.replace(/\bno-js\b/, "")
+  })`,
+      }}
+    />,
+  ])
 }
