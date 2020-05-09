811f69995c6aec8bc512e76c186660f02f0303d2
Mohammed 'Mo' Mulazada <thisismoniac@gmail.com>
2020-05-09 22:58:36 +0200

    F

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Portfolio | Mohammed Mulazada" />
     <Header />
-    <div className="px-10">
+    <div className="px-10 pt-16">
     <GradientHeading headingLevel={2}>
       My <br /> work
     </GradientHeading>
