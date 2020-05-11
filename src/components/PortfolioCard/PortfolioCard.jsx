85aa8c1ef832a30c54657787bf53b5e653e5628d
Mohammed 'Mo' Mulazada <thisismoniac@gmail.com>
2020-05-11 21:18:36 +0200

    add work

--- src/components/PortfolioCard/PortfolioCard.jsx
+++ src/components/PortfolioCard/PortfolioCard.jsx
@@ -4,7 +4,15 @@ import containerStyles from './PortfolioCard.module.css'
 const PortfolioCard = props => {
 
     return (
-        <div className={`${containerStyles.PortfolioCard} flex w-full justify-center items-center hover:shadow-outline focus:shadow-outline`}>
+        <div className={`${containerStyles.PortfolioCard} p-8 flex flex-col justify-between w-full hover:shadow-outline focus:shadow-outline`}>
+            <div className="flex flex-col">
+                <h4>Blank</h4>
+                <p>Together with: Company</p>
+            </div>
+
+            <div>
+                <p className="italic">Blank is a website about. About two sentences.</p>
+            </div>
             {/* <p className={`${containerStyles.PortfolioCard__description}`}>test</p> */}
             {/* <img src="https://ruler.nl/app/themes/ruler/dist/images/ruler-logo_d1ce0e5f.svg" /> */}
             {/* <div className={`${containerStyles.ocean} ocean`}>
