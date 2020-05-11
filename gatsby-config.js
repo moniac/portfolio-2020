49f3b6366755e79e5452489d3f7d701ce6b6141b
Mohammed 'Mo' Mulazada <thisismoniac@gmail.com>
2020-05-12 00:18:38 +0200

    pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,8 +66,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-optimize-svgs`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
