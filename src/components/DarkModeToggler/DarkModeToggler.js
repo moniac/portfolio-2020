5d726bd13319fe0187dd46929dad53d982aa4d95
Mohammed 'Mo' Mulazada <thisismoniac@gmail.com>
2020-05-16 22:11:32 +0200

    change moon and sun icon

--- src/components/DarkModeToggler/DarkModeToggler.js
+++ src/components/DarkModeToggler/DarkModeToggler.js
@@ -1,31 +1,38 @@
-import React from 'react';
-import gradientMoon from '../../images/icons8-moon-and-stars-96.png'
-import gradientSun from '../../images/icons8-smiling-sun-96.png'
+import React from "react"
+import gradientMoon from "../../images/icons8-moon-and-stars-96.png"
+import gradientSun from "../../images/icons8-smiling-sun-96.png"
 // import sunIcon from '../../images/icons8-sun-96.png'
 // import moonIcon from '../../images/icons8-crescent-moon-96.png'
-import containerStyles from './DarkModeToggler.module.css'
+import containerStyles from "./DarkModeToggler.module.css"
 
-import { ThemeContext } from '../ThemeContext';
+import { ThemeContext } from "../ThemeContext"
 
 const DarkModeToggler = () => {
-  const { colorMode, setColorMode } = React.useContext(ThemeContext);
+  const { colorMode, setColorMode } = React.useContext(ThemeContext)
 
   if (!colorMode) {
-    return null;
+    return null
   }
 
   return (
     <label className={containerStyles.DarkModeToggler}>
       <input
         type="checkbox"
-        checked={colorMode === 'dark'}
+        checked={colorMode === "dark"}
         onChange={ev => {
-          setColorMode(ev.target.checked ? 'dark' : 'light');
+          setColorMode(ev.target.checked ? "dark" : "light")
         }}
       />
-      <img alt={colorMode === 'dark' ? 'A moon icon to toggle the websites dark mode' : 'A sun icon to toggle the websites dark mode'} src={colorMode === 'dark' ? gradientMoon : gradientSun}/>
+      <img
+        alt={
+          colorMode === "light"
+            ? "A moon icon to toggle the websites dark mode"
+            : "A sun icon to toggle the websites dark mode"
+        }
+        src={colorMode === "light" ? gradientMoon : gradientSun}
+      />
     </label>
-  );
-};
+  )
+}
 
-export default DarkModeToggler;
+export default DarkModeToggler
