eb2f5303c6e414420aa7ce23a876ac8b1444a953
Mohammed 'Mo' Mulazada <thisismoniac@gmail.com>
2020-05-23 03:12:01 +0200

    fix

--- src/components/Progress/Progress.jsx
+++ src/components/Progress/Progress.jsx
@@ -2,28 +2,19 @@ import React, { useEffect, useRef } from 'react';
 import containerStyles from './Progress.module.css';
 
 const Progress = props => {
-  useEffect(() => {
-    if (typeof window === 'undefined' || !window.document) {
-      console.log(
-        `bailing out of the useeffect. Going to continue to render??`
-      );
-      return;
-    }
-  }, []);
   const progressBar = useRef(null);
 
-  var h = document?.documentElement,
-    b = document?.body,
-    st = 'scrollTop',
-    sh = 'scrollHeight',
-    scroll;
-
-  function update() {
-    scroll = ((h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight)) * 100;
-    progressBar.current.style.setProperty('--scroll', scroll + '%');
-  }
-
   useEffect(() => {
+    var h = document?.documentElement,
+      b = document?.body,
+      st = 'scrollTop',
+      sh = 'scrollHeight',
+      scroll;
+
+    function update() {
+      scroll = ((h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight)) * 100;
+      progressBar.current.style.setProperty('--scroll', scroll + '%');
+    }
     document.addEventListener('scroll', update);
 
     return () => document.removeEventListener('scroll', update);
