142edbf290a733d8e04a27ecdc31955c85de1ce8
Mohammed 'Mo' Mulazada <thisismoniac@gmail.com>
2020-05-23 12:26:25 +0200

    i

--- src/components/DarkModeToggler/AnimatedDarkModeToggler.jsx
+++ src/components/DarkModeToggler/AnimatedDarkModeToggler.jsx
@@ -101,7 +101,7 @@ const AnimatedDarkModeToggler = props => {
             light: { r: '58px', rotate: 180 },
           }}
           animate={colorMode === 'dark' ? 'light' : 'dark'}
-          initial={'light'}
+          initial={colorMode}
         />
         <Circle
           cx="250"
