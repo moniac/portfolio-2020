8cc3a91468911d4203934df3d7692cb0bc7f552f
Mohammed 'Mo' Mulazada <thisismoniac@gmail.com>
2020-05-23 10:33:26 +0200

    hoi

--- src/components/DarkModeToggler/AnimatedDarkModeToggler.jsx
+++ src/components/DarkModeToggler/AnimatedDarkModeToggler.jsx
@@ -112,7 +112,7 @@ const AnimatedDarkModeToggler = props => {
           }}
           animate={animateValue}
           initial={false}
-          fill={colorMode === 'dark' ? L'hsl(210deg, 38%, 15%)' : white}
+          fill={colorMode === 'dark' ? 'hsl(210deg, 38%, 15%)' : 'white'}
           fillOpacity={colorMode === 'dark' ? '1' : '0'}
         />
       </motion.svg>
