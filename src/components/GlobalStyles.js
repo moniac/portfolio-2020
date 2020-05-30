d761b98f38923f20e231a4572e8518c7a7bd4017
Mohammed 'Mo' Mulazada <thisismoniac@gmail.com>
2020-05-30 17:38:40 +0200

    fix transition

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -14,13 +14,6 @@ const GlobalStyles = createGlobalStyle`
     color: var(--color-text);
   }
 
-  body.transition {
-    @media (prefers-reduced-motion: no-preference) {
-      transition: color 300ms ease-out, fill 300ms ease-out, stroke 300ms ease-out, background-color 300ms ease-out;
-  }
-
-
-
   h1, h2, h3, h4, h5, h6, p {
     -webkit-font-smoothing: antialiased;
     scroll-margin-top: 4rem;
