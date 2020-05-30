bea309c507b4779886c22bf34f8ad7a4a2fe4ca9
Mohammed 'Mo' Mulazada <thisismoniac@gmail.com>
2020-05-30 17:28:33 +0200

    fix width

--- src/Layouts/BlogLayout.jsx
+++ src/Layouts/BlogLayout.jsx
@@ -1,15 +1,15 @@
-import React from "react"
-import containerStyles from "./BlogLayout.module.css"
+import React from 'react';
+import containerStyles from './BlogLayout.module.css';
 
 const BlogLayout = props => {
-  const { children } = props
+  const { children } = props;
   return (
     <div
       className={`${containerStyles.BlogLayout} BlogLayout container mx-auto max-w-3xl py-8 lg:py-24 px-8`}
     >
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default BlogLayout
+export default BlogLayout;
