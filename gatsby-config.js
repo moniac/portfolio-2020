bcca641cad8cdfc35986c6fb9e3a5095bb62d24a
Mohammed 'Mo' Mulazada <thisismoniac@gmail.com>
2020-06-02 15:49:13 +0200

    remove styled

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,6 @@ module.exports = {
     author: `@thisismoniac`,
   },
   plugins: [
-    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
